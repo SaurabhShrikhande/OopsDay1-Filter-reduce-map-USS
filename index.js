148ae9b6dba81a9ef1d1b0cb2004e253a1392691
Saurabh Shrikhande <saurabhshrikhande200@gmail.com>
2023-11-23 15:37:16 +0530

    USS

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+// Writing our custom filter(mineFilter) which work exactly same as array methods(filter)
+// Writing our custom reduce(mineReduce) which work exactly same as array methods(reduce)
+// Writing our custom map(mineMap) which work exactly same as array methods(map)
+// You have to complete these questions. You have to create the custom functions.
+
+
 // Custom mineFilter function
 function mineFilter(array, callback) {
   const filteredArray = [];
